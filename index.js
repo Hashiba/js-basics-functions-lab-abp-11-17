f1542480ff36af4b925293b69f836ecec3795602
training@hashiba.org <training@hashiba.org>
2017-12-17 22:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file!
 // function calculateVertical(start, destination){
 //  return (destination - start)*264;}
-
 function distanceFromHqInBlocks(start){
   return Math.abs(start -42);
 }
@@ -16,5 +15,28 @@ return (Math.abs(start -destination))*264;
 
 function calculatesFarePrice(start, destination){
 
-
+if (distance < 400){
+  let fare = 0;
 }
+
+if (distance >= 400 && distance < 2000){
+  let fare = (distance *2)/100;
+}
+
+if (distance >= 2000 && distance < 2500){
+  let fare = 25;
+}
+
+if (distance >= 2500){
+  let fare = 25;
+}
+
+return fare;
+}
+
+calculatesFarePrice: Given the same starting and ending block, 
+return the fare for the customer. The first four hundred feet are free. 
+For a distance between 400 and 2000 feet, the price is 2 cents per foot. 
+Then Scuber charges a flat fare for a distance over 2000 feet and under 2500 feet. 
+Finally Scuber does not allow any rides over 2500 feet 
+- the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
