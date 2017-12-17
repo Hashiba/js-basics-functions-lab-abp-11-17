c0b3cf133a2adb0266c2a5a9d28455e1dc8cc5c1
training@hashiba.org <training@hashiba.org>
2017-12-17 22:23:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+// function calculateVertical(start, destination){
+//  return (destination - start)*264;}
+
+function distanceFromHqInBlocks(start){
+  return start -42;
+}
+
+function distanceFromHqInFeet(){
+
+}
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(start, destination){
+
+
+}
